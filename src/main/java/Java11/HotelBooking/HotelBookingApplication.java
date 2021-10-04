bf6dbf197548dc3658e0f61a0604be75ae0d289c
Trà Hữu Minh <trahuuminh@gmail.com>
2021-10-04 10:44:04 +0700

    Initial commit

---
+++ src/main/java/Java11/HotelBooking/HotelBookingApplication.java
@@ -0,0 +1,13 @@
+package Java11.HotelBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingApplication.class, args);
+	}
+
+}
