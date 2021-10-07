eb65f344973f3661f376567d8b2dbce6ce95c566
Tanduong3005 <nhattan87264@gmail.com>
2021-10-07 14:44:04 +0700

    created CRUD Amenities

---
+++ src/main/java/nhom8/javabackend/hotel/hotel/dto/AmenitiesDto.java
@@ -0,0 +1,20 @@
+package nhom8.javabackend.hotel.hotel.dto;
+
+public interface AmenitiesDto {
+	
+	public Long getId();
+	
+	public int getGuestRoom();
+	
+	public int getBedRoom();
+	
+	public boolean getWifiAvailability();
+	
+	public boolean getParkingAvailability();
+	
+	public boolean getPoolAvailability();
+	
+	public boolean getAirCondition();
+	
+	public boolean getExtraBedFacility();
+}
